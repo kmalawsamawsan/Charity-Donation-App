0378e4b6faafe6a72aa0f14ccc338aeeb54fc356
kmalawsan <kmalawsan611@gmail.com>
2025-02-27 14:18:35 +0300

    إصلاح خطأ في بناء الجملة في src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { NotificationsProvider } from './context/NotificationsContext'; // استيراد NotificationsProvider
+import { NotificationsProvider } from './context/NotificationsContext'; // تأكد من وجود هذا الملف والمسار صحيح
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
